3d1211825dd1a4137f39c5aafc2cf3e9623f04ef
sauravs152002 <sauravs152002@gmail.com>
2026-05-03 22:20:46 +0530

    Updated project files

--- portfoliofiles/src/app/Home.tsx
+++ portfoliofiles/src/app/Home.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router';
 import { ImageWithFallback } from './components/figma/ImageWithFallback';
 import { LiquidGlassCursor } from './components/LiquidGlassCursor';
 import { projects } from './data/projects';
+import riyaImage from './images/riya.jpeg';
 
 export default function Home() {
   const { scrollY } = useScroll();
@@ -233,10 +234,10 @@ export default function Home() {
                 className="relative aspect-[4/5] w-full max-w-[400px] overflow-hidden rounded-[32px] bg-black/5"
               >
                 <ImageWithFallback 
-                  src="https://images.unsplash.com/photo-1606162094541-2ea11d0dd56b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxtaW5pbWFsaXN0JTIwcG9ydHJhaXQlMjB3b21hbiUyMGRlc2lnbmVyfGVufDF8fHx8MTc3NzM4MzI2MHww&ixlib=rb-4.1.0&q=80&w=1080" 
-                  alt="Riya" 
-                  className="w-full h-full object-cover grayscale-[30%]"
-                />
+  src={riyaImage}
+  alt="Riya" 
+  className="w-full h-full object-cover grayscale-[30%]"
+/>
               </motion.div>
             </div>
           </div>
